3c4131e69a499e3a44ff419202015051fcbde872
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-02-08 22:31:34 +0530

    update APP.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import HomePage from "./pages/client-pages/homePage.jsx";
 import AdminPage from "./pages/admin-pages/admin.jsx";
 import { BrowserRouter,Route,Routes } from "react-router-dom";
+import Test from "./components/test.jsx";
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <BrowserRouter>
     <Routes>
     <Route path="/admin/*" element={<AdminPage/>}/>
+    <Route path="/test" element={<Test/>}/>
     <Route path="/*" element={<HomePage/>}/>
     </Routes>
     </BrowserRouter>
