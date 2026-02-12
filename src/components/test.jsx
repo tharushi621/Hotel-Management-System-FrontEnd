e2ef63046197e6daff045fb2338f844f6c081bc1
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-02-12 20:46:40 +0530

    updaqte codes

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -1,7 +1,17 @@
+import uploadMedia from "../utils/mediaUpload"
+import { useState } from "react"
+
 export default function TestPage(){
+const [file,setFile]=useState(null)
+
     return(
         <div className="w-full h-[100vh] flex justify-center items-center">
-            
+            <input type="file" defaultValue={file} onChange={(e)=>{
+                setFile(e.target.files[0])
+            }}/>
+            <button onClick={()=>{
+                uploadMedia(file)
+            }}>Submit</button>
         </div>
     )
 }
---
+++ src/config/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAb90WxN2z_bXdtg9Kr2J44EyMdNm7eQpI",
+  authDomain: "hmsbe-86eaa.firebaseapp.com",
+  projectId: "hmsbe-86eaa",
+  storageBucket: "hmsbe-86eaa.firebasestorage.app",
+  messagingSenderId: "1035857812663",
+  appId: "1:1035857812663:web:3ab665c91903ab522f0132",
+  measurementId: "G-RHLLPJ7G1Z"
+};
+const app = initializeApp(firebaseConfig);
+
+export default app;
