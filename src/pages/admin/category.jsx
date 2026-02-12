2d08fd1a5b3fed3a29717cc4327652e81d4a45d2
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-02-12 20:45:08 +0530

    update category file

--- src/pages/admin/category.jsx
+++ src/pages/admin/category.jsx
@@ -1,4 +1,17 @@
+import { useState, useEffect } from "react"
+
 export default function AdminCategory(){
+
+const [categories,setCategories]=useState([])
+const [categoriesIsLoaded,setCategoriesIsLoaded]=useState(false)
+
+useEffect(()=>{
+  axios.get(import.meta.env.VITE_BACKEND_URL+"/api/category").then((res)=>{
+    console.log(res)
+  })
+},[])
+
+
     return(
         <div className="w-full">
             Category
