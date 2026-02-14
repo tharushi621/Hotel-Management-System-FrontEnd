82439ee283b38a5512b0fa30835f0b384b0c687d
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-02-14 20:48:11 +0530

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,19 @@ import { BrowserRouter,Route,Routes } from "react-router-dom";
 import Loginpage from "./pages/login.jsx";
 import AdminCategories from "./pages/client-pages/categories.jsx";
 import TestPage from "./components/test.jsx";
+import {Toaster} from 'react-hot-toast';
+import AdminCategory from "./pages/admin/category.jsx";
 
 function App() {
 
   return (
    
     <BrowserRouter>
+    <Toaster position="top-right" reverseOrder={false}/>
     <Routes>
     <Route path="/admin/*" element={<AdminPage/>}/>
     <Route path = "/login" element={<Loginpage/>}/>
-    <Route path = "/categories" element={<AdminCategories/>}/>
+    <Route path = "/categories" element={<AdminCategory/>}/>
     <Route path = "/test" element={<TestPage/>}/>
     <Route path="/*" element={<HomePage/>}/>
     </Routes>
