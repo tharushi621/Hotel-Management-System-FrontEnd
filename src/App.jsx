8ab74746998801e8dc9c16e852d6b1701d4eb87e
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-02-17 10:11:40 +0530

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,35 @@
 import "./App.css";
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import HomePage from "./pages/client-pages/homePage.jsx";
 import AdminPage from "./pages/admin-pages/admin.jsx";
-import { BrowserRouter,Route,Routes } from "react-router-dom";
 import Loginpage from "./pages/login.jsx";
-import AdminCategories from "./pages/client-pages/categories.jsx";
-import TestPage from "./components/test.jsx";
-import {Toaster} from 'react-hot-toast';
 import AdminCategory from "./pages/admin/category.jsx";
+import AdminBooking from "./pages/admin/booking.jsx";
+import TestPage from "./components/test.jsx";
+import LeonineSplash from "./components/LeonineSplash.jsx";
 
 function App() {
+  const [showSplash, setShowSplash] = useState(true);
 
   return (
-   
-    <BrowserRouter>
-    <Toaster position="top-right" reverseOrder={false}/>
-    <Routes>
-    <Route path="/admin/*" element={<AdminPage/>}/>
-    <Route path = "/login" element={<Loginpage/>}/>
-    <Route path = "/categories" element={<AdminCategory/>}/>
-    <Route path = "/test" element={<TestPage/>}/>
-    <Route path="/*" element={<HomePage/>}/>
-    </Routes>
-    </BrowserRouter>
+    <>
+      {showSplash && <LeonineSplash onComplete={() => setShowSplash(false)} />}
 
-  )
+      <BrowserRouter>
+        <Toaster position="top-right" reverseOrder={false} />
+        <Routes>
+          <Route path="/admin/*" element={<AdminPage />} />
+          <Route path="/login" element={<Loginpage />} />
+          <Route path="/categories" element={<AdminCategory />} />
+          <Route path="/bookings" element={<AdminBooking />} />
+          <Route path="/test" element={<TestPage />} />
+          <Route path="/*" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
