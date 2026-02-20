5acd6f3642fb36678ae4818185ccb1252c084ab5
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-02-20 23:00:41 +0530

    update

--- src/pages/admin/updateCategoryForm.jsx
+++ src/pages/admin/updateCategoryForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import { uploadMedia } from "../utils/uploadMedia";
+import uploadMedia from "../../utils/uploadMedia";
 
 export default function UpdateCategoryForm() {
   const { state } = useLocation();
