d4a176993e4d176bd8b0e4ed952e29c6e9087235
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-02-23 00:58:29 +0530

    update

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -25,6 +25,7 @@ export default function Loginpage() {
           navigate("/admin");
         } else {
           const redirectTo = location.state?.from || "/";
+          console.log("STATE:", location.state, "REDIRECT:", redirectTo);
           navigate(redirectTo);
         }
       })
